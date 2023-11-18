ae7d0d090794aca0bfdccd96a49c6336b06954b1
mosehmaguah <you@example.com>
2023-11-19 02:24:31 +0300

    2d mtrix rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Define a function that rotates an nxn 2D matrix 90 degrees clockwise in-place
+    Defines a function that rotates an nxn 2D matrix 90 degrees clockwise in-place
 """
 
 
